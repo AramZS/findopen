73dbc27d019177b294f47f8f08295a90ca6acbf2
Aram Zucker-Scharff <azuckers@gmu.edu>
2013-04-06 13:06:25 -0400

    setting up data crawler

--- room-crawler.php
+++ room-crawler.php
@@ -6,3 +6,19 @@ require_once('simple_html_dom.php');
 $dom = new simple_html_dom;
 
 $contentHtml = file_get_html();
+
+$contentHtml = file_get_html('http://localhost:8080/xampp/findopen/tables.html');
+//set_error_handler("customError");
+
+foreach ($html->find('table') as $table){
+	$c = 0;
+	foreach ($table->find('tr') as $row){
+	
+		if ($c > 1){
+			
+		}
+		$c++;
+	
+	}
+
+}
